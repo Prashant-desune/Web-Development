bec3f672671ee46b983ffa96902b675492598aee
Prashant Kumar <prashant.desune@gmail.com>
2022-05-22 11:02:18 +0530

    Create if-else.js

---
+++ JavaScript/if-else.js
@@ -0,0 +1,17 @@
+//Check wether a person is able to vote or not 
+// Condition is if the person is 18 or 18+
+
+let age ;
+age = parseInt(prompt('Enter the age :'));
+if(age >= 18){
+    console.log("you can vote!")
+}
+else{
+    console.log("you can't vote!")
+}
+
+//check the code using the link 
+
+--------------------------------
+<script src="if-else.js"></script>
+--------------------------------
